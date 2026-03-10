54cb748f8ce2a30dd6da8303489e274e93f00548
Hard2Recall <simicandrija1306@gmail.com>
2026-03-10 19:35:39 +0100

    spelling changes

--- SHAhashGenerator/Version1/SHAhash.py
+++ SHAhashGenerator/Version1/SHAhash.py
@@ -3,17 +3,17 @@ import getpass
 
 password = input("Enter your password: ")
 
-# Generisi SHA-1 hash za datu sifru
+# Generates SHA-1 hash for your password
 hash_obj = hashlib.sha1()
 hash_obj.update(password.encode())
 sha1_hash = hash_obj.hexdigest()
 
-# Generisi SHA-256 hash za datu sifru
+# Generates SHA-256 hash for your password
 hash_obj = hashlib.sha256()
 hash_obj.update(password.encode())
 sha256_hash = hash_obj.hexdigest()
 
-# Generisi SHA-512 hash za datu sifru
+# Generates SHA-512 hash for your password
 hash_obj = hashlib.sha512()
 hash_obj.update(password.encode())
 sha512_hash = hash_obj.hexdigest()
